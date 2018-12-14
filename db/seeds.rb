7c4ab630573f92ed4f0605a63f9d0e63d9a4d5be
Benjamin Schwind <mazlgoth@gmail.com>
2018-12-13 17:51:22 -0800

    customized routes and adjusted seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
 10.times do |post|
-  
+  Post.create!(
+  	thumb_image: "https://place-hold.it/500x400",
+		title: "Costume #{post}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+			dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+			fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+		
+
+		image: "https://place-hold.it/700x1000"
+  	)
 end
+
+puts "10 posts created"
